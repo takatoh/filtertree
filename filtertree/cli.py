4519327d4de5a0d90470fa70376e9a1d078e6784
takatoh <takatoh.m@gmail.com>
2024-10-16 20:50:28 +0900

    filtertree/cli.py: New.

---
+++ filtertree/cli.py
@@ -0,0 +1,45 @@
+from filtertree import __version__
+import os
+from pathlib import Path
+import subprocess
+import argparse
+
+
+def main():
+    args = parse_arguments()
+
+    src_dir = args.src_dir
+    dest_dir = Path(args.dest_dir)
+    for cur_dir, _, files in os.walk(src_dir):
+        current = Path(cur_dir)
+        for file in files:
+            src_file_path = current / file
+            dest_file_path = dest_dir.joinpath(src_file_path.relative_to(src_dir))
+            dest_file_path.parent.mkdir(parents=True, exist_ok=True)
+            cmd = f"echo {str(src_file_path)} {str(dest_file_path)}"
+            #print(cmd)
+            subprocess.call(cmd, shell=True)
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "src_dir",
+        metavar="SRCDIR",
+        action="store",
+        help="source directory"
+    )
+    parser.add_argument(
+        "dest_dir",
+        metavar="DESTDIR",
+        action="store",
+        help="destination directory"
+    )
+    parser.add_argument(
+        "-V", "--version",
+        action="version",
+        version=f"v{__version__}",
+        help="show version and exit"
+    )
+    args = parser.parse_args()
+    return args
