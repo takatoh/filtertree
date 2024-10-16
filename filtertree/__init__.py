2874a6d024fbbb7f4ce4b8d8ad996611d27f6117
takatoh <takatoh.m@gmail.com>
2024-10-16 22:35:39 +0900

    filtertree/__init__.py(filtertree()): New.

--- filtertree/__init__.py
+++ filtertree/__init__.py
@@ -1,1 +1,16 @@
+from pathlib import Path
+import os
+
 __version__ = "0.1.0"
+
+
+def filtertree(func, src_dir, dest_dir):
+    src_dir = Path(src_dir)
+    dest_dir = Path(dest_dir)
+    for cur_dir, _, files in os.walk(src_dir):
+        current = Path(cur_dir)
+        for file in files:
+            src_file_path = current / file
+            dest_file_path = dest_dir.joinpath(src_file_path.relative_to(src_dir))
+            dest_file_path.parent.mkdir(parents=True, exist_ok=True)
+            func(src_file_path, dest_file_path)
