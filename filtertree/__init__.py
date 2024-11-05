76d97fa6991bc3fdd29a8c8151c26375b1c4a29c
takatoh <takatoh.m@gmail.com>
2024-11-05 21:49:30 +0900

    filtertree/__init__.py(filtertree()): Tweak.

--- filtertree/__init__.py
+++ filtertree/__init__.py
@@ -7,10 +7,10 @@ __version__ = "0.1.0"
 def filtertree(func, src_dir, dest_dir):
     src_dir = Path(src_dir)
     dest_dir = Path(dest_dir)
-    for cur_dir, _, files in os.walk(src_dir):
-        current = Path(cur_dir)
+    for curr, _, files in os.walk(src_dir):
+        current_dir = Path(curr)
         for file in files:
-            src_file_path = current / file
+            src_file_path = current_dir / file
             dest_file_path = dest_dir / src_file_path.relative_to(src_dir)
             dest_file_path.parent.mkdir(parents=True, exist_ok=True)
             func(src_file_path, dest_file_path)
