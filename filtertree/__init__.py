01c537dda1f8f864539bc18e03ebddcc926ddf6e
takatoh <takatoh.m@gmail.com>
2025-01-09 05:10:50 +0900

    filtertree/__init__.py(filtertree()): Add argument 'overwrite'. Overwrite the destination files.

--- filtertree/__init__.py
+++ filtertree/__init__.py
@@ -4,7 +4,7 @@ import os
 __version__ = "0.1.0"
 
 
-def filtertree(func, src_dir, dest_dir):
+def filtertree(func, src_dir, dest_dir, overwrite=False):
     src_dir = Path(src_dir)
     dest_dir = Path(dest_dir)
     for curr, _, files in os.walk(src_dir):
@@ -12,5 +12,7 @@ def filtertree(func, src_dir, dest_dir):
         for file in files:
             src_file_path = current_dir / file
             dest_file_path = dest_dir / src_file_path.relative_to(src_dir)
+            if dest_file_path.exists() and not overwrite:
+                continue
             dest_file_path.parent.mkdir(parents=True, exist_ok=True)
             func(src_file_path, dest_file_path)
